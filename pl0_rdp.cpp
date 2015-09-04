ac19f7f3b8eeefc13245dc04dc776b725473f7f7
JiadingGai <paul.paul.mit@gmail.com>
2015-09-03 23:30:38 -0400

    Add a prototype for gettok() function.

--- pl0_rdp.cpp
+++ pl0_rdp.cpp
@@ -52,3 +52,31 @@ void program(void);
 
 static std::string IdentifierStr;
 static double NumVal;
+
+static int gettok()
+{
+  static int LastChar = ' ';
+
+  while (isspace(LastChar))
+    LastChar = getchar();
+
+  if (isalpha(LastChar)) {
+    IdentifierStr = LastChar;
+    while (isalnum(LastChar = getchar())) {
+      IdentifierStr += LastChar;
+    }
+
+#if _DEBUG
+    printf("[IdentifierStr] %s (followed by %c)\n", IdentifierStr.c_str(), LastChar);
+#endif
+
+    if (IdentifierStr == "call") return callsym;
+    if (IdentifierStr == "begin") return beginsym;
+    if (IdentifierStr == "end") return endsym;
+    if (IdentifierStr == "if") return ifsym;
+    if (IdentifierStr == "while") return whilesym;
+    if (IdentifierStr == "then") return thensym;
+    if (IdentifierStr == "do") return dosym;
+  }
+
+}
